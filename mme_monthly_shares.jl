23210907ba5da0ee127ccb1f24901cd906270e04
Austin B. Bean <austinbean@gmail.com>
2019-08-28 16:46:40 -0400

    nearly there

--- mme_monthly_shares.jl
+++ mme_monthly_shares.jl
@@ -3,23 +3,72 @@ using Dates, CSV, Tables
 abstract type substance_month end
 abstract type zipcode_market end 
 
+# top level is : dict{ COUNTY_YEAR, monthly_mme}
+
+
+
+struct prod_year <: zipcode_market
+	d::Dict{Dates.Year, Array{Float64,1}} # this dict will hold active ingredient, MME by months/year.
+end 
 
 struct monthly_mme <: substance_month 
-	h::Dict{String,Float64} # a substance and a mme quantity
+	h::Dict{String,prod_year} # a substance and a mme quantity
 end 
 
-struct zip_mkt <: zipcode_market
-	d::Dict{Date, monthly_mme} # this dict will hold active ingredient, MME by months/year.
-end 
+# Base: Dict{String, zip_mkt}
 
-# change this to handle years...?  It makes more sense to organize it that way, I think.  
+# Different way: County_state -> substance -> month -> quantity.
+
+#  Dict{ 
+#       County_state(string){ 
+#                            Dict{ 
+#                                  Substance(String) 
+#                                                   Dict{
+#                                                         Year, Array{Float64,1}} } }
+
+
+
+
+function get_mme(x, outp::Dict{String, monthly_mme} ; datecol = 31, subscol = 36, countycol = 20, pill_count_coll = 25, statecol = 18, mmefactcol = 38,  quant_ingr_col = 32)
+	dateformat = Dates.DateFormat("mmddyyyy")
+	for i = 1:size(x, 1)
+		county = x[i,countycol]                                                                          # location is a unique county/state pair as a string - what if it's missing?
+		state = x[i, statecol]
+		location = county*"_"*state
+		# TODO - there are some missings in quant_ingr_col -> set to zero
+		mme_equiv = x[i,mmefactcol]*x[i,pill_count_coll]*x[i, quant_ingr_col]                            # TODO - this isn't right yet, but let's get a version to start with.  - is MME by weight or by pill unit? 
+		if ismissing(x[i,quant_ingr_col])
+			mme_equiv = 0.0
+		end 
+		substance = x[i,subscol]
+		date_month = Dates.month(Dates.Date( x[i, datecol] , dateformat))                                # CAUTION - Dates.month and Dates.Month are different functions.  Dates.month: Dates.Date -> Month (as an Int64)
+			dm = Dates.Month(Dates.Date( x[i, datecol] , dateformat))
+		date_year = Dates.Year(Dates.Date(x[i, datecol], dateformat))
+		purch_date = Dates.Date(dm, date_year) 
+		if haskey(outp, location)                                                     # if the state/county pair has been seen before, outp[location].h[string] -> a substance
+			if haskey(outp[location].h, substance)                                    # the substance has been seen before.
+				if haskey(outp[location].h[substance].d, date_year)                   # substance has been seen in that year.
+					outp[location].h[substance].d[date_year][date_month] += mme_equiv # month will be an index 1-12
+				else 
+					outp[location].h[substance].d[date_year] = zeros(Float64,12)
+					outp[location].h[substance].d[date_year][date_month] += mme_equiv             # date_month will be a 1-12 index
+				end 
+			else                                                                   # the substance has not been seen in that county before.
+				outp[location].h[substance] = prod_year( Dict(date_year => zeros(Float64,12)) )
+			end 
+		else                                                                
+			outp[location] = monthly_mme( Dict( substance => prod_year( Dict( date_year => zeros(Float64,12))) ) ) # if the state/county pair has not been seen before
+			outp[location].h[substance].d[date_year][date_month] += mme_equiv 
+		end
+	end 
+end 
 
 
 # Test these types:
 # Load a subset: 		
-# f = CSV.read("/Users/tuk39938/Downloads/arcos_chunk1.csv"; header = 1, limit = 5000,  types=Dict("REPORTER_ZIP"=>String, "BUYER_ZIP"=>String, "TRANSACTION_DATE"=>String ))
+# f = CSV.read("/Users/tuk39938/Downloads/arcos_chunk1.csv"; header = 1, limit = 5000,  types=Dict("REPORTER_ZIP"=>String, "BUYER_ZIP"=>String, "TRANSACTION_DATE"=>String, "CALC_BASE_WT_IN_GM"=>Float64 ))
 # outp = Dict{String, zip_mkt}()
-# get_mme(f, outp)
+# get_mme(f)
 
 # Next steps - 
 # this must be reorgnized... really I need to know what substances appear in a month, then if they are there in the month, what's the amount?  
@@ -32,61 +81,73 @@ zip_mkt = Dict{County_State, Object}
 Object = Month_Year -> {Ingredient_Name, Quantity}
 
 =#
-	# (2) Get shares from this.  
 
 
-function get_mme(x, outp::Dict{String, zip_mkt} ; datecol = 31, subscol = 36, countycol = 20, pill_count_coll = 25, statecol = 18, mmefactcol = 38,  quant_ingr_col = 32)
-	dateformat = Dates.DateFormat("mmddyyyy")
-	for i = 1:size(x, 1)
-		county = x[i,countycol]                                                                          # location is a unique county/state pair as a string - what if it's missing?
-		state = x[i, statecol]
-		location = county*"_"*state
-		mme_equiv = x[i,mmefactcol]*x[i,pill_count_coll]*x[i, quant_ingr_col]                            # TODO - this isn't right yet, but let's get a version to start with.  - is MME by weight or by pill unit? 
-		substance = x[i,subscol]
-		date_month = Dates.Month(Dates.Date( x[i, datecol] , dateformat))                                # CAUTION - Dates.month and Dates.Month are different functions.  Dates.month: Dates.Date -> Month (as an Int64)
-		date_year = Dates.Year(Dates.Date(x[i, datecol], dateformat))
-		purch_date = Dates.Date(date_month, date_year) 
-		if haskey(outp, location)                                                                        # if the state/county pair has been seen before
-			if haskey(outp[location].d, purch_date)
-				if haskey(outp[location].d[purch_date].h, substance)
-					outp[location].d[purch_date].h[substance] += mme_equiv
-				else 
-					outp[location].d[purch_date].h[substance] = mme_equiv
-				end 
-			else
-				outp[location].d[purch_date] = monthly_mme( Dict(substance => mme_equiv) )
-			end 
-		else                                                                
-			outp[location] = zip_mkt( Dict( purch_date => monthly_mme( Dict( substance => mme_equiv)) )) # if the state/county pair has not been seen before
-		end 
-	end 
-	return outp 
-end 
 
 
-function mme_output(x::Dict{String,zip_mkt}, year::Int64) # try to write this out by year...
+function mme_output(x::Dict{String,monthly_mme}) # try to write this out by year...
 	ctr::Int64 = 0
 	# just record drugs with positive market share - add extras later, but be aware of market entry.  
-	zip_dims = length(keys(x)) # this is not right.  There are many substances per zip-month!
+	zip_dims = length(keys(x)) 
 	labels::Int64 = 3
 	months::Int64 = 12
 	years::Int64 = 7
-	outp_array = Array{Union{String, Float64}}(undef, zip_dims, labels+months)
+	subs_num::Int64 = 65
+	outp_array = Array{Union{String, Number}}(undef, (zip_dims*subs_num), labels+months, years)
 	for i in eachindex(outp_array)
 		outp_array[i] = 0
 	end
+	# this is going to really suck... (zips × max subs) x (months + labels) × years = 7 
 	# output should be: for each zip (col 1), drug (col 2), 12 months quantity (3 - 14) 
 	# so there are something like: 20_000 zip codes, 12 months, some quantity of drug ingredients, 7 years.
-	rowctr = 1
-	for k in keys(x)           # this is the county_state 
-		for dt in keys(x[k].d) # these are dates as Month-Year
-			if Dates.Year(dt) == year 
-				yr = Dates.Year(dt)
-				mnth = Dates.Month(dt)
-				for sub in keys(x[k].d[dt])
-					outp_array[rowctr, 1] = k
-					outp_array[rowctr, 2] = substance
-					outp_array[rowctr, 3] = yr
+	rowctr2006 = 0; rowctr2007 = 0; rowctr2008 = 0; rowctr2009 = 0; rowctr2010 = 0; rowctr2011 = 0; rowctr2012 = 0
+	for k in keys(x)                     # this is the county_state 
+		for sub in keys(x[k].h)          # these are substances as strings
+			for year in keys(x[k].h[sub].d)    # year as a Dates.Year
+				rowctr = 0
+				if year == Dates.Year(2006)
+					yrix = 1
+					yrval = 2006
+					rowctr2006 += 1
+					rowctr = rowctr2006
+				elseif year == Dates.Year(2007)
+					yrix = 2
+					yrval = 2007
+					rowctr2007 += 1
+					rowctr = rowctr2007
+				elseif year == Dates.Year(2008)
+					yrix = 3
+					yrval = 2008
+					rowctr2008 += 1
+					rowctr = rowctr2008
+				elseif year == Dates.Year(2009)
+					yrix = 4 
+					yrval = 2009
+					rowctr2009 += 1
+					rowctr = rowctr2009
+				elseif year == Dates.Year(2010)
+					yrix = 5
+					yrval = 2010
+					rowctr2010 += 1
+					rowctr = rowctr2010
+				elseif year == Dates.Year(2011)
+					yrix = 6
+					yrval = 2011
+					rowctr2011 += 1
+					rowctr = rowctr2011
+				elseif year == Dates.Year(2012)
+					yrix = 7
+					yrval = 2012
+					rowctr2012 += 1
+					rowctr = rowctr2012
+				else
+					throw("vomit") 
+				end 
+				outp_array[rowctr, 1, yrix] = k
+				outp_array[rowctr, 2, yrix] = sub
+				outp_array[rowctr, 3, yrix] = yrval
+				for mth = 1:12
+					outp_array[rowctr, mth+labels, yrix] += x[k].h[sub].d[year][mth]
 				end 
 			end 
 		end 
@@ -95,24 +156,80 @@ function mme_output(x::Dict{String,zip_mkt}, year::Int64) # try to write this ou
 end 
 
 
+
 # Real data test 
 function get_mme_data(x)
-	mk1 = Dict{String, zip_mkt}()
+	mk1 = Dict{String, monthly_mme}()
+	maxl = 0
 	for i = 1:8
 		println("Loading $i  ", now())
-		f = CSV.read("/Users/tuk39938/Downloads/arcos_chunk$i.csv"; header = 1, limit = 500,  types=Dict("REPORTER_ZIP"=>String, "BUYER_ZIP"=>String, "TRANSACTION_DATE"=>String ))
+		f = CSV.read("/Users/tuk39938/Downloads/arcos_chunk$i.csv"; header = 1, limit = 500_000, types=Dict("REPORTER_ZIP"=>String, "BUYER_ZIP"=>String, "TRANSACTION_DATE"=>String , "CALC_BASE_WT_IN_GM"=>Float64, "CALC_BASE_WT_IN_GM"=>Float64))
 		println("Processing $i  ", now())
 		get_mme(f, mk1)
 		f = 0 # drop
 	end
-	return mk1
-	# println("Writing Output ", now())
-	# smth = output_array(mk1)
+	println("Writing Output ", now())
+	smth = mme_output(mk1)
 	# println("Saving ", now())
-	# CSV.write("/Users/tuk39938/Desktop/programs/ops/short_test.csv", Tables.table(smth); header=["zip_code", "substance", "date", "ctr"])
+	# CSV.write("/Users/tuk39938/Desktop/programs/ops/yearly_write_test_2006.csv", Tables.table(smth[:,:, 1]); header=["COUNTY", "SUBSTANCE", "YEAR", "JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"])
+	# CSV.write("/Users/tuk39938/Desktop/programs/ops/yearly_write_test_2007.csv", Tables.table(smth[:,:, 2]); header=["COUNTY", "SUBSTANCE", "YEAR", "JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"])
+	# CSV.write("/Users/tuk39938/Desktop/programs/ops/yearly_write_test_2008.csv", Tables.table(smth[:,:, 3]); header=["COUNTY", "SUBSTANCE", "YEAR", "JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"])
+	# CSV.write("/Users/tuk39938/Desktop/programs/ops/yearly_write_test_2009.csv", Tables.table(smth[:,:, 4]); header=["COUNTY", "SUBSTANCE", "YEAR", "JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"])
+	# CSV.write("/Users/tuk39938/Desktop/programs/ops/yearly_write_test_2010.csv", Tables.table(smth[:,:, 5]); header=["COUNTY", "SUBSTANCE", "YEAR", "JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"])
+	# CSV.write("/Users/tuk39938/Desktop/programs/ops/yearly_write_test_2011.csv", Tables.table(smth[:,:, 6]); header=["COUNTY", "SUBSTANCE", "YEAR", "JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"])
+	# CSV.write("/Users/tuk39938/Desktop/programs/ops/yearly_write_test_2012.csv", Tables.table(smth[:,:, 7]); header=["COUNTY", "SUBSTANCE", "YEAR", "JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"])
+	return smth
 end 
 
 
+function to_shares(arr1::Array)
+	# takes a three dim array and creates shares within columns over blocks of rows 
+	rows = size(arr1,1)
+	labels = 3
+	i = 1; j = 1;
+	while arr1[i,1] != 0.0
+		str1 = arr1[i, 1] # find the first county name 
+		j = i # reassign j 
+		while (arr1[j,1] == str1)&(arr1[j,1]!=0.0)
+			str_last = j
+			j = j+1
+		end 
+		println(i, " ", j, " ", str1)
+		# TODO - ends up being off by 1.  
+		# TODO - this doesn't end up actually doing anything...?  
+		for mnth = 1:12
+			norm_const = sum(arr1[i:j,mnth+labels])
+			for ix = i:j
+				arr1[ix,mnth+labels] = arr1[ix,mnth+labels]/norm_const
+			end 
+		end 
+		i = j #reassign i 
+		i+=1 
+	end 
+end 
+
+
+"""
+`mme_dim_counter`
+max number of substances in any county-month
+"""
+function mme_dim_counter(d)
+	# Max substances in any county month is 65 in Broward, FL, 2011-12-01
+	max_dim::Int64 = 0
+	county = ""
+	month = Dates.Date("01012001", "ddmmyyyy")
+	for k1 in keys(d)            # these are county_state 
+		for k2 in keys(d[k1].d)  # these are dates 
+			# max month count clearly 12
+			if length(keys(d[k1].d[k2].h)) > max_dim 
+				max_dim = length(keys(d[k1].d[k2].h))
+				month = k2
+				county = k1 
+			end 
+		end 
+	end 
+	return max_dim, month, county 
+end 
 
 function year_min_max(x)
 	dateformat = Dates.DateFormat("mmddyyyy")
@@ -144,9 +261,8 @@ end
 # min year is 2006, max year is 2012.
 
 
+# run functions 
+
+get_mme_data(1)
 
 
-
-
-
-#
