4d0ed30d2e037c0b9cf41d5c1a492dc3c707d348
Austin Bean <austinbean@gmail.com>
2021-09-01 10:49:13 -0400

    loose tolerance but a version which converges

--- pyblp_drugs.py
+++ pyblp_drugs.py
@@ -8,18 +8,54 @@ product_data = pd.read_csv("/Users/austinbean/Desktop/programs/opioids/pyblp_tes
 consumer_data = pd.read_csv("/Users/austinbean/Desktop/programs/opioids/py_blp_demographics.csv")
 
 
+# TODO - 
+# 1 higher number of iterations on delta    
+# 2 parallel over markets  
+
 # try the plain vanilla logit formulation first.  
+    # product_data has the demand instruments
+
+logit_f = pyblp.Formulation('prices', absorb='C(ndc_code)')
+logit_p = pyblp.Problem(logit_f, product_data)
+logit_r = logit_p.solve()
+        # add package size
+logit_f2 = pyblp.Formulation('prices + package')
+logit_p2 = pyblp.Problem(logit_f2, product_data)
+logit_r2 = logit_p2.solve()
+        # active ingredient.  
+logit_f3 = pyblp.Formulation('prices + ingred')
+logit_p3 = pyblp.Problem(logit_f3, product_data)
+logit_r3 = logit_p3.solve()
+        # package, active ingredient
+logit_f4 = pyblp.Formulation('prices + ingred + package')
+logit_p4 = pyblp.Problem(logit_f4, product_data)
+logit_r4 = logit_p4.solve()
 
 # Random coefficients formulation ... 
-X1_formulation = pyblp.Formulation('1 + prices')
-X2_formulation = pyblp.Formulation('1 + mme + package')
-product_formulations = (X1_formulation, X2_formulation)
-mc_integration = pyblp.Integration('monte_carlo', size=50, specification_options={'seed': 0})
+    # could put package in non random coeff.
+    # adding a 1 to X1 formulation "works" but no SEs
+    # having just prices + package w/ NDC fe's gives no SEs
+X1_formulation = pyblp.Formulation('1 + prices + mme + package')
+X2_formulation = pyblp.Formulation('0 + prices + mme + package')
+
+
+    # MC integration
+product_formulations = (X1_formulation, X2_formulation) 
+mc_integration = pyblp.Integration('monte_carlo', size=500, specification_options={'seed': 0})
 mc_problem = pyblp.Problem(product_formulations, product_data, integration=mc_integration)
 bfgs = pyblp.Optimization('bfgs', {'gtol': 1e-4})
-results1 = mc_problem.solve(sigma=np.ones((3, 3)), optimization=bfgs)
+results1 = mc_problem.solve(sigma=np.eye(3), optimization=bfgs)
 
+    # iteration:   NOT USED YET.  Maybe not needed...
+    # TODO - adjusting delta iterations bound 
+    # iteration = pyblp.Iteration('squarem', {'norm': np.linalg.norm, 'scheme': 1})
 
 agent_formulation = pyblp.Formulation('0 + male + hhinc + unemp')
 demo_problem = pyblp.Problem(product_formulations, product_data, agent_formulation, consumer_data)
 results2 = mc_problem.solve(sigma = np.eye(3), optimization=bfgs)
+
+
+   # exact integration
+#pr_integration = pyblp.Integration('product', size=5)
+#pr_problem = pyblp.Problem(product_formulations, product_data, integration=pr_integration)
+#pr_problem.solve(sigma=np.eye(3), optimization=bfgs)
