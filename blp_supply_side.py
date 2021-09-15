677112414adc0c6949e2f3ffb4c0a1005f413d3c
Austin Bean <austinbean@gmail.com>
2021-09-15 16:33:55 -0400

    figured out concentration problem

--- blp_supply_side.py
+++ blp_supply_side.py
@@ -1,5 +1,6 @@
 # production
-
+# Takes about 2 hours at a very loose tolerance.  Optimization does not converge
+# in step 1.
 
 import pyblp
 import pandas as pd
@@ -10,13 +11,27 @@ product_data = pd.read_csv("/Users/austinbean/Desktop/programs/opioids/pyblp_tes
 consumer_data = pd.read_csv("/Users/austinbean/Desktop/programs/opioids/py_blp_demographics.csv")
 
 
-X1_formulation = pyblp.Formulation('0 + prices', absorb='C(ndc_code)')   # linear
+X1_formulation = pyblp.Formulation('1 + mme')                            # linear / taking prices out of this line
 X2_formulation = pyblp.Formulation('1 + prices + mme + package')         # non-linear
 X3_formulation = pyblp.Formulation('1 + mme + package')                  # supply-side
 product_formulations = (X1_formulation, X2_formulation, X3_formulation)
-agent_formulation = pyblp.Formulation('0 + male + hhinc + unemp')
+agent_formulation = pyblp.Formulation('0 + male + hhinc')  # + unemp
 supply_problem = pyblp.Problem(product_formulations, product_data, agent_formulation, consumer_data)
 bfgs = pyblp.Optimization('bfgs', {'gtol': 1e-4})
-intial_sigma = np.eye(4)
-initial_pi = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 0, 0]])
+initial_sigma = np.eye(4)        # length X2 
+initial_pi = np.random.rand(4,2) # length X2 x length agent_formulation
 results = supply_problem.solve(sigma=initial_sigma,pi=initial_pi, costs_bounds=(0.001, None), optimization=bfgs)
+
+costs = results.compute_costs()
+markups = results.compute_markups(costs=costs)
+
+# Merger sim
+hhi = results.compute_hhi()
+profits = results.compute_profits(costs=costs)
+cs = results.compute_consumer_surpluses()
+
+product_data['merger_ids'] = '78' # suppose merge to monopoly.  
+
+changed_prices = results.compute_approximate_prices(firm_ids=product_data['merger_ids'],costs=costs)
+
+changed_shares = results.compute_shares(changed_prices)
