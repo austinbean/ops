e7e41f7f34fb7fa879d4436cf547bbdbe62017db
Austin Bean <austinbean@gmail.com>
2021-10-19 14:22:37 -0400

    added an additional demographic

--- blp_supply_side.py
+++ blp_supply_side.py
@@ -16,7 +16,7 @@ X1_formulation = pyblp.Formulation('1 + mme')                            # linea
 X2_formulation = pyblp.Formulation('1 + prices + mme + package')         # non-linear
 X3_formulation = pyblp.Formulation('1 + mme + package')                  # supply-side
 product_formulations = (X1_formulation, X2_formulation, X3_formulation)
-agent_formulation = pyblp.Formulation('0 + male + hhinc + unemp')        # options: male, race, disability, hhinc, education, labor, unemp
+agent_formulation = pyblp.Formulation('0 + male + hhinc + unemp + education')        # options: male, race, disability, hhinc, education, labor, unemp
 supply_problem = pyblp.Problem(product_formulations, product_data, agent_formulation, consumer_data)
 bfgs = pyblp.Optimization('l-bfgs-b', {'gtol': 1e-14})
 
@@ -24,7 +24,7 @@ initial_sigma = np.eye(4)        # length X2
 sigma_lower = (-3)*np.eye(4)
 sigma_upper = 3*np.eye(4)
 
-initial_pi = np.random.rand(4,3) # length X2 x length agent_formulation
+initial_pi = np.random.rand(4,4) # length X2 x length agent_formulation
 
 iteration_options = pyblp.Iteration(method='squarem', method_options={'max_evaluations': 200000})
 with pyblp.parallel(10):
